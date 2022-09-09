2dff0206af8aae6b2744f11ce9912e188ff0e9b2
Karolina.Vaittinen <karolinavaittinen@gmail.com>
2022-09-09 19:02:31 +0200

    Progres1

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ window.addEventListener("scroll", function () {
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const selector = ".menu__link";
+  const elems = Array.from(document.querySelectorAll(selector));
+  const navigation = document.querySelector("nav");
+
+  function makeActive(evt) {
+    const target = evt.target;
+
+    if (!target || !target.matches(selector)) {
+      return;
+    }
+
+    elems.forEach((elem) => elem.classList.remove("active"));
+
+    evt.target.classList.add("active");
+  }
+
+  navigation.addEventListener("mousedown", makeActive);
+});
